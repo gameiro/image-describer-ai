5c26c155c524ff50723a4780d85634312e948f9a
Carlos <c_gameiro@hotmail.com>
2023-12-16 22:45:10 +0200

    Added sockets
    Signed-off-by: Carlos <c_gameiro@hotmail.com>

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
-from flask import Flask, Response
-import subprocess
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from subprocess import Popen, PIPE
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
-def trigger_job():
-    # Execute another Python file or script here and capture the output
+def index():
+    return render_template('index.html')
+
+def generate_logs():
     try:
-        process = subprocess.Popen(['python', 'images_ai_blog_generator.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        stdout, stderr = process.communicate()
-        logs = f"STDOUT:\n{stdout}"
-        
-        if process.returncode == 0:  # Check if the subprocess ran successfully
-            return Response(f'Job triggered successfully!\n\n{logs}', content_type='text/plain')
-        else:
-            return Response(f'Error: Unable to trigger the job\n\n{logs}', content_type='text/plain'), 500
-    except subprocess.CalledProcessError as e:
-        return f'Error: Unable to trigger the job\n{e}', 500
+        process = Popen(['python', 'images_ai_blog_generator.py'], stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True)
+
+        for stdout_line in iter(process.stdout.readline, ''):
+            socketio.emit('log', {'data': stdout_line})
+        process.stdout.close()
+
+        for stderr_line in iter(process.stderr.readline, ''):
+            socketio.emit('log', {'data': stderr_line})
+        process.stderr.close()
+
+        return_code = process.wait()
+        socketio.emit('log', {'data': f'Process completed with return code {return_code}'})
+    except Exception as e:
+        socketio.emit('log', {'data': f'Error: {str(e)}'})
+
+@socketio.on('connect')
+def test_connect():
+    socketio.emit('log', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('trigger_job')
+def trigger_job():
+    socketio.start_background_task(target=generate_logs)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    socketio.run(app, debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
