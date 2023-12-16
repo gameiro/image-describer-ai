658d4d17a629ba518fb5fda8fe0e1d34dd5d4587
Carlos <c_gameiro@hotmail.com>
2023-12-16 21:49:07 +0200

    Run job in the background correctly
    Signed-off-by: Carlos <c_gameiro@hotmail.com>

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def trigger_job():
-    # Execute another Python file or script here
-    try:
-        subprocess.run(['python', 'images_ai_blog_generator.py'], check=True)
-        return 'Job triggered successfully!'
-    except subprocess.CalledProcessError:
-        return 'Error: Unable to trigger the job'
+    # Return a message indicating that the job is running
+    start_background_job()
+    return 'Job triggered. It is running in the background.'
+
+def start_background_job():
+    # Execute another Python file or script here in the background
+    subprocess.Popen(['python', 'images_ai_blog_generator.py'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
