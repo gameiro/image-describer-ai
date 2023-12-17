0f55e0532a1a78a21b3de57d88b30e423a05a6a3
Carlos <c_gameiro@hotmail.com>
2023-12-17 19:14:03 +0200

    Fix blocked context issue
    Signed-off-by: Carlos <c_gameiro@hotmail.com>

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def index():
@@ -41,4 +41,4 @@ def trigger_job():
     socketio.start_background_task(target=generate_logs)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    socketio.run(app, debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), ssl_context='adhoc')
